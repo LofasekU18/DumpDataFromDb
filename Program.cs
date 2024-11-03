c1db15f4912b26e259a71d1c3f47585aa1c7b317
LofasekU18 <lstrnad13@gmail.com>
2024-11-03 16:25:14 +0100

    On Win-86x not working open specific txt, need to figure out, something todo, after that start creating server/client version of program

--- Program.cs
+++ Program.cs
@@ -10,62 +10,64 @@ using System.Runtime.Caching;
 using System.Collections.Concurrent;
 
 
-
-const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\data\\odposta.mdb;";
+//TODO create Enviroment file or config file for access to connectionString and query
+// const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\data\\odposta.mdb;";   //local debug
+const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=J:\\odposta.mdb;";
 const string query = $"SELECT Číslo, Code, Adresát,[Kód úkonu] FROM [Kniha úkonov];";
 string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\output.txt";
-List<string> listOfKodUkonu = ["-2120063737", "-2052727437", "-2030258641", "-1917318598", "-1847281444", "-1808742348", "-1796241568", "-1786773114", "-1748838384", "-1694504395", "-1668446977", "-1665351517", "-1517761113", "-1505536988", "-1483927677", "-1412285989", "-1344265705", "-1267306452", "-1232565484", "-1217505685", "-1210935807", "-1107823104", "-1106413256", "-1084376640", "-1071305888", "-1063233504", "-701744320", "-619113172", "-590050635", "-494699571", "-276545597", "-238707365", "-212811081", "-91742702", "-87103988", "-36279323", "-25053951", "22", "24", "26", "28", "30", "32", "34", "89", "95", "106414959", "134550253", "152214958", "228590420", "280548393", "457680707", "465487351", "585958650", "695949508", "711819427", "751146987", "791230221", "823153261", "894890524", "935444745", "1197348685", "1303495360", "1609235670", "1626423248", "1768096735", "1920555376", "1940675200", "2058691153", "-2095445506", "-1925324557", "1024815598"];
+System.Console.WriteLine(filePath);
 
+//TODO : change to dictonary and add name of Ukonu
+List<string> listOfKodUkonu = ["-2120063737", "-2052727437", "-2030258641", "-1917318598", "-1847281444", "-1808742348", "-1796241568", "-1786773114", "-1748838384", "-1694504395", "-1668446977", "-1665351517", "-1517761113", "-1505536988", "-1483927677", "-1412285989", "-1344265705", "-1267306452", "-1232565484", "-1217505685", "-1210935807", "-1107823104", "-1106413256", "-1084376640", "-1071305888", "-1063233504", "-701744320", "-619113172", "-590050635", "-494699571", "-276545597", "-238707365", "-212811081", "-91742702", "-87103988", "-36279323", "-25053951", "22", "24", "26", "28", "30", "32", "34", "89", "95", "106414959", "134550253", "152214958", "228590420", "280548393", "457680707", "465487351", "585958650", "695949508", "711819427", "751146987", "791230221", "823153261", "894890524", "935444745", "1197348685", "1303495360", "1609235670", "1626423248", "1768096735", "1920555376", "1940675200", "2058691153", "-2095445506", "-1925324557", "1024815598"];
+Stopwatch sw = new Stopwatch();
+IEnumerable<ExekucniPrikazy> selectedDataList = new List<ExekucniPrikazy>();
 try
 {
-    Stopwatch sw = new Stopwatch();
     sw.Start();
-    List<ExekucniPrikazy> listData = await GetListOfExe(connectionString, query);
-    var selectedDataList = listData.Where(zaznam => listOfKodUkonu.Contains(zaznam.KodUkonu));
+    System.Console.WriteLine("Načítám úkony z DB");
+    List<ExekucniPrikazy> listData = await GetListOfExeAsync(connectionString, query);
+    selectedDataList = listData.Where(zaznam => listOfKodUkonu.Contains(zaznam.KodUkonu));
     sw.Stop();
     Console.WriteLine($"Vyhledat to trvalo: {sw.Elapsed}");
     sw.Restart();
-    while (true)
-    {
-        Console.WriteLine("Zadej exko, 'k' pro ukonceni");
-        string exko = Console.ReadLine();
-        if (exko == "k")
-        {
-            break;
-        }
-        else
-        {
-            try
-            {
-                sw.Start();
-                var selectedDataListExko = selectedDataList.Where(ex => ex.Exko == exko).OrderBy(ex => ex.Cislo);
-                await WriteListOfExe(filePath, exko, selectedDataListExko);
-                sw.Stop();
-                Console.WriteLine($"Vyhledat to zapsat: {sw.Elapsed}");
-                Console.WriteLine("Data byla připsána do souboru.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Problém se souborem, tento problém:" + ex.Message);
-            }
-        }
-        Console.ReadLine();
-        using (Process process = Process.Start(filePath))
-        {
-            process.WaitForExit();
-        }
-        Console.Clear();
-    }
 }
 catch (Exception ex)
 {
     Console.WriteLine("SQL problem: " + ex.Message);
 }
+while (true)
+{
+    Console.WriteLine("Zadej exko, 'k' pro ukonceni");
+    string exko = Console.ReadLine();
+    if (exko == "k")
+    {
+        break;
+    }
+    else
+    {
+        try
+        {
+            sw.Start();
+            var selectedDataListExko = selectedDataList.Where(ex => ex.Exko == exko).OrderBy(ex => ex.Cislo);
+            await WriteListOfExeAsync(filePath, exko, selectedDataListExko);
+            sw.Stop();
+            Console.WriteLine($"Vyhledat to zabralo: {sw.Elapsed}");
+            Console.WriteLine("Data byla připsána do souboru.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Problém se souborem, tento problém:" + ex.Message);
+        }
+    }
+    await StartAndWaitForProcessAsync(@"notepad.exe """ + filePath + @"""");
+    System.Console.WriteLine("Hotovo, ENTER");
+    Console.ReadLine();
+    System.Console.Clear();
 
 
+}
 
-
-static async Task<List<ExekucniPrikazy>> GetListOfExe(string connectionString, string query)
+async Task<List<ExekucniPrikazy>> GetListOfExeAsync(string connectionString, string query)
 {
     List<ExekucniPrikazy> dataList = new List<ExekucniPrikazy>();
     using (OleDbConnection connection = new OleDbConnection(connectionString))
@@ -92,7 +94,7 @@ static async Task<List<ExekucniPrikazy>> GetListOfExe(string connectionString, s
     return dataList;
 
 }
-static async Task WriteListOfExe(string path, string exko, IOrderedEnumerable<ExekucniPrikazy> selectedDataList)
+async Task WriteListOfExeAsync(string path, string exko, IEnumerable<ExekucniPrikazy> selectedDataList)
 {
     using (StreamWriter writer = new StreamWriter(path, true))
     {
@@ -101,9 +103,38 @@ static async Task WriteListOfExe(string path, string exko, IOrderedEnumerable<Ex
         await writer.WriteLineAsync();
         foreach (var data in selectedDataList)
         {
-            string line = $"{data.Cislo} {data.Adresat}";
-            await writer.WriteLineAsync(line);
+            await writer.WriteLineAsync($"{data.Cislo} {data.Adresat}");
+        }
+    }
+}
+async Task StartAndWaitForProcessAsync(string processName)
+{
+    using (Process process = new Process())
+    {
+        process.StartInfo.FileName = processName;
+        process.StartInfo.UseShellExecute = false;
+        System.Console.WriteLine(process.StartInfo.FileName.ToString());
+        try
+        {
+            process.Start();
+            // Console.WriteLine($"Program {processName} zapnut."); => debug
 
+            await process.WaitForExitAsync();
+
+            // Console.WriteLine($"Program {processName} vypnut."); => debug
+        }
+        // catch (InvalidOperationException ex)
+        // {
+        //     Console.WriteLine("The process was already exited or not started properly."); //debug
+        //     Console.WriteLine("Error: " + ex.Message);
+        // }
+        // catch (System.IO.IOException ex)
+        // {
+        //     Console.WriteLine("IOException occurred: " + ex.Message); //debug
+        // }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Problém se s externí aplikací " + ex.Message);
         }
     }
 }
@@ -112,7 +143,5 @@ class ExekucniPrikazy
     public string Exko { get; init; }
     public int Cislo { get; init; }
     public string Adresat { get; init; }
-
     public string KodUkonu { get; init; }
-
 }
