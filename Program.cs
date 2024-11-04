3887473f67170eed277b69be7b9b33c1a5bbacf9
LofasekU18 <lstrnad13@gmail.com>
2024-11-04 13:58:25 +0100

    Working version, consume a lot of memory ~250MB

--- Program.cs
+++ Program.cs
@@ -9,15 +9,16 @@ using System.Linq;
 using System.Runtime.Caching;
 using System.Collections.Concurrent;
 
-
-//TODO create Enviroment file or config file for access to connectionString and query
-// const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\data\\odposta.mdb;";   //local debug
+//TODO: 
+//create Enviroment file or config file for access to connectionString and query
+//NOTE: local debug
+//const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\data\\odposta.mdb;";
 const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=J:\\odposta.mdb;";
 const string query = $"SELECT Číslo, Code, Adresát,[Kód úkonu] FROM [Kniha úkonov];";
 string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\output.txt";
-System.Console.WriteLine(filePath);
 
-//TODO : change to dictonary and add name of Ukonu
+//TODO: 
+//change to dictonary and add name of Ukonu
 List<string> listOfKodUkonu = ["-2120063737", "-2052727437", "-2030258641", "-1917318598", "-1847281444", "-1808742348", "-1796241568", "-1786773114", "-1748838384", "-1694504395", "-1668446977", "-1665351517", "-1517761113", "-1505536988", "-1483927677", "-1412285989", "-1344265705", "-1267306452", "-1232565484", "-1217505685", "-1210935807", "-1107823104", "-1106413256", "-1084376640", "-1071305888", "-1063233504", "-701744320", "-619113172", "-590050635", "-494699571", "-276545597", "-238707365", "-212811081", "-91742702", "-87103988", "-36279323", "-25053951", "22", "24", "26", "28", "30", "32", "34", "89", "95", "106414959", "134550253", "152214958", "228590420", "280548393", "457680707", "465487351", "585958650", "695949508", "711819427", "751146987", "791230221", "823153261", "894890524", "935444745", "1197348685", "1303495360", "1609235670", "1626423248", "1768096735", "1920555376", "1940675200", "2058691153", "-2095445506", "-1925324557", "1024815598"];
 Stopwatch sw = new Stopwatch();
 IEnumerable<ExekucniPrikazy> selectedDataList = new List<ExekucniPrikazy>();
@@ -28,7 +29,7 @@ try
     List<ExekucniPrikazy> listData = await GetListOfExeAsync(connectionString, query);
     selectedDataList = listData.Where(zaznam => listOfKodUkonu.Contains(zaznam.KodUkonu));
     sw.Stop();
-    Console.WriteLine($"Vyhledat to trvalo: {sw.Elapsed}");
+    Console.WriteLine($"Načtení trvalo: {sw.Elapsed}");
     sw.Restart();
 }
 catch (Exception ex)
@@ -59,12 +60,10 @@ while (true)
             Console.WriteLine("Problém se souborem, tento problém:" + ex.Message);
         }
     }
-    await StartAndWaitForProcessAsync(@"notepad.exe """ + filePath + @"""");
-    System.Console.WriteLine("Hotovo, ENTER");
-    Console.ReadLine();
+    await StartAndWaitForProcessAsync("notepad.exe", filePath);
+    System.Console.WriteLine("Hotovo");
+    Thread.Sleep(1000);
     System.Console.Clear();
-
-
 }
 
 async Task<List<ExekucniPrikazy>> GetListOfExeAsync(string connectionString, string query)
@@ -107,34 +106,21 @@ async Task WriteListOfExeAsync(string path, string exko, IEnumerable<ExekucniPri
         }
     }
 }
-async Task StartAndWaitForProcessAsync(string processName)
+async Task StartAndWaitForProcessAsync(string processName, string filePath)
 {
     using (Process process = new Process())
     {
         process.StartInfo.FileName = processName;
+        process.StartInfo.Arguments = filePath;
         process.StartInfo.UseShellExecute = false;
-        System.Console.WriteLine(process.StartInfo.FileName.ToString());
         try
         {
             process.Start();
-            // Console.WriteLine($"Program {processName} zapnut."); => debug
-
             await process.WaitForExitAsync();
-
-            // Console.WriteLine($"Program {processName} vypnut."); => debug
         }
-        // catch (InvalidOperationException ex)
-        // {
-        //     Console.WriteLine("The process was already exited or not started properly."); //debug
-        //     Console.WriteLine("Error: " + ex.Message);
-        // }
-        // catch (System.IO.IOException ex)
-        // {
-        //     Console.WriteLine("IOException occurred: " + ex.Message); //debug
-        // }
         catch (Exception ex)
         {
-            Console.WriteLine("Problém se s externí aplikací " + ex.Message);
+            Console.WriteLine("Problém s externí aplikací " + ex.Message);
         }
     }
 }
