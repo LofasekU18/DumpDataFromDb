9220b03bfd48409a943d5cf9db07644b11bc95a4
Lukáš Strnad <lstrnad13@gmail.com>
2024-11-08 09:29:56 +0100

    git : Need to repair merge pull and names of commits, code : add Task.Run() with token.Cancel() to cancel querry, need to figure out, how exactly it's working

--- Program.cs
+++ Program.cs
@@ -9,34 +9,52 @@ using System.Linq;
 using System.Runtime.Caching;
 using System.Collections.Concurrent;
 
-//TODO: 
-//create Enviroment file or config file for access to connectionString and query
+//TODO: create Enviroment file or config file for access to connectionString and query
+
 //NOTE: local debug
-//const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\data\\odposta.mdb;";
-const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=J:\\odposta.mdb;";
+const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\data\\odposta.mdb;";
+// const string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=J:\\odposta.mdb;";
 const string query = $"SELECT Číslo, Code, Adresát,[Kód úkonu] FROM [Kniha úkonov];";
 string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\output.txt";
-
+CancellationTokenSource cancel = new CancellationTokenSource();
+CancellationToken token = cancel.Token;
+bool loadCancelled = false;
 //TODO: 
 //change to dictonary and add name of Ukonu
 List<string> listOfKodUkonu = ["-2120063737", "-2052727437", "-2030258641", "-1917318598", "-1847281444", "-1808742348", "-1796241568", "-1786773114", "-1748838384", "-1694504395", "-1668446977", "-1665351517", "-1517761113", "-1505536988", "-1483927677", "-1412285989", "-1344265705", "-1267306452", "-1232565484", "-1217505685", "-1210935807", "-1107823104", "-1106413256", "-1084376640", "-1071305888", "-1063233504", "-701744320", "-619113172", "-590050635", "-494699571", "-276545597", "-238707365", "-212811081", "-91742702", "-87103988", "-36279323", "-25053951", "22", "24", "26", "28", "30", "32", "34", "89", "95", "106414959", "134550253", "152214958", "228590420", "280548393", "457680707", "465487351", "585958650", "695949508", "711819427", "751146987", "791230221", "823153261", "894890524", "935444745", "1197348685", "1303495360", "1609235670", "1626423248", "1768096735", "1920555376", "1940675200", "2058691153", "-2095445506", "-1925324557", "1024815598"];
 Stopwatch sw = new Stopwatch();
 IEnumerable<ExekucniPrikazy> selectedDataList = new List<ExekucniPrikazy>();
+Task.Run(() =>
+        {
+            Console.WriteLine("Press any key to cancel...");
+            Console.ReadKey();
+            cancel.Cancel();
+            loadCancelled = true;
+
+        });
 try
 {
     sw.Start();
     System.Console.WriteLine("Načítám úkony z DB");
-    List<ExekucniPrikazy> listData = await GetListOfExeAsync(connectionString, query);
+    List<ExekucniPrikazy> listData = await GetListOfExeAsync(connectionString, query, token);
     selectedDataList = listData.Where(zaznam => listOfKodUkonu.Contains(zaznam.KodUkonu));
     sw.Stop();
     Console.WriteLine($"Načtení trvalo: {sw.Elapsed}");
     sw.Restart();
 }
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Query was canceled by user.");
+}
 catch (Exception ex)
 {
     Console.WriteLine("SQL problem: " + ex.Message);
 }
-while (true)
+finally
+{
+    cancel.Dispose();
+}
+while (loadCancelled == false)
 {
     Console.WriteLine("Zadej exko, 'k' pro ukonceni");
     string exko = Console.ReadLine();
@@ -66,7 +84,7 @@ while (true)
     System.Console.Clear();
 }
 
-async Task<List<ExekucniPrikazy>> GetListOfExeAsync(string connectionString, string query)
+async Task<List<ExekucniPrikazy>> GetListOfExeAsync(string connectionString, string query, CancellationToken token)
 {
     List<ExekucniPrikazy> dataList = new List<ExekucniPrikazy>();
     using (OleDbConnection connection = new OleDbConnection(connectionString))
@@ -87,6 +105,7 @@ async Task<List<ExekucniPrikazy>> GetListOfExeAsync(string connectionString, str
             };
             dataList.Add(data);
         }
+        token.ThrowIfCancellationRequested();
 
         await reader.CloseAsync();
     }
